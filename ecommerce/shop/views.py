1353cddd45ac7cb5f5cea8ef2a6034c9ae10c65c
zwados <lukasz.rozwadowski7@gmail.com>
2022-09-23 02:50:46 +0200

    load bootstrap template, create main.html, shop-main.html, navbar.html, footer.html templates; create shopMainPage view and add urls

--- ecommerce/shop/views.py
+++ ecommerce/shop/views.py
@@ -3,4 +3,4 @@ from django.shortcuts import render
 # Create your views here.
 def shopMainPage(request):
     context = {}
-    return render(request, 'main.html', context)
+    return render(request, 'shop/shop-main.html', context)
